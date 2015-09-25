665e9f8da61b3e854064e43199373def03a1d755
Michael Wenocur <mlwenocur@users.noreply.github.com>
2015-09-25 00:25:42 -0700

    reset trainer input defaults and finished draft users guide

--- ui.R
+++ ui.R
@@ -3,13 +3,13 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel('Logistic Regression Trainer'),
     sidebarPanel(
-        numericInput('sampsPerTrial', 'A: Input sample size', 100, min=20, max =20000, step = 10),
+        numericInput('sampsPerTrial', 'A: Input sample size', 1000, min=20, max =20000, step = 10),
         selectInput('selectedModel', 'B: Model Selector',
                            c('Polynomial Model' = 'poly',
                              'Harmonic Model' = 'harmonic'),
                            'poly'),
-        numericInput('minX', 'C: Input X min value', 0.3),
-        numericInput('maxX', 'D: Input X max value', 5),
+        numericInput('minX', 'C: Input X min value', 1, step = 0.1),
+        numericInput('maxX', 'D: Input X max value', 3),
         numericInput('userSeed', 'E: Input seed', 222),
         selectInput('grView', 'F: Graph Type', c('Log Odds Graph' = 'rho',
                                               'Probability Graph' = 'prob'), 
